2fa84903c214ab53336f3e274a8188da21339b7d
Kevin Adams <kevin_adams25@hotmail.com>
2018-04-25 14:59:26 +0200

    Override OIS to set singleton at load

--- MemberTest/src/model/MyDate.java
+++ MemberTest/src/model/MyDate.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
@@ -39,4 +41,12 @@ public class MyDate implements Serializable{
 	public String toString() {
 		return getDay() + "/" + (getMonth()+1) + "/" + getYear();
 	}
+	private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException{
+		ois.defaultReadObject();
+		myDate = this;
+	}
+	private Object readResolve() {
+		return myDate;
+	}
+	
 }
